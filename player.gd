69e9e8680f8a0bce1f99a4491dfe5c22ad39b559
Flabberghasted <notclyde39@gmail.com>
2026-03-26 20:26:20 +1300

    feat: added health bar

--- player.gd
+++ player.gd
@@ -2,15 +2,15 @@ extends Area2D
 
 signal hit
 @export var speed = 400
+var health
+@export var max_health = 100
 var screen_size
 var is_dead: bool
 
-# Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	screen_size = get_viewport_rect().size
 	#hide()
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
 	if is_dead: return
 	var vel = Vector2.ZERO
@@ -27,22 +27,27 @@ func _process(delta: float) -> void:
 		vel = vel.normalized() * speed
 		position += vel * delta
 		position = position.clamp(Vector2.ZERO, screen_size)
-		$AnimatedSprite2D.play()
+		$Sprites.play()
 	else:
-		$AnimatedSprite2D.stop()
+		$Sprites.stop()
 	if vel.x < 0:
-		$AnimatedSprite2D.flip_h = true
+		$Sprites.flip_h = true
 	elif vel.x > 0:
-		$AnimatedSprite2D.flip_h = false
+		$Sprites.flip_h = false
 
-func _on_body_entered(body: Node2D) -> void:
+func _on_body_entered(_body: Node2D) -> void:
+	health -= 25
+	$HealthBar.value = health * 100 / max_health
+	if health > 0: return
 	is_dead = true
 	hide()
 	hit.emit()
-	$CollisionShape2D.set_deferred("disabled", true)
+	$Hitbox.set_deferred("disabled", true)
 
 func start(pos: Vector2):
 	position = pos
 	is_dead = false
+	health = 100
+	$HealthBar.value = 100
 	show()
-	$CollisionShape2D.disabled = false
+	$Hitbox.disabled = false
